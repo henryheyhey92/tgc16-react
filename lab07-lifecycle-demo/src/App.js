047a48cac3d98fa16214d851c6e58dd9208d63dc
kunxin-chor <chorkunxin@yahoo.com>
2022-03-11 05:47:34 +0000

    Added lifecycle demo

--- lab07-lifecycle-demo/src/App.js
+++ lab07-lifecycle-demo/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Child from "./Child"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    // in JavaScript, if we do inheritance, we have call the parent
+    // class constructor
+    super();  // <-- call the parent class constructor
+    console.log("App's constructor is called");
+  }
+
+  state = {
+    'number': 0
+  }
+
+  render() {
+    console.log("App's render is called");
+    return <React.Fragment>
+      <h1>App</h1>
+      <Child/>
+      <Child/>
+    </React.Fragment>
+  }
+  componentDidMount() {
+    // will only be once
+    console.log("App's componentDidMount is called");
+
+  }
 }
 
 export default App;
