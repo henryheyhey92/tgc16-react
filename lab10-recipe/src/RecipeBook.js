76520c0c8a7924bc1b7682b83f50ac9e40f12f62
kunxin-chor <chorkunxin@yahoo.com>
2022-03-16 06:06:10 +0000

    Added axios for recipe app

--- lab10-recipe/src/RecipeBook.js
+++ lab10-recipe/src/RecipeBook.js
@@ -1,24 +1,47 @@
 import React from 'react'
 import Listing from "./Listing";
 import AddNew from "./AddNew"
+import axios from 'axios'
 
 export default class RecipeBook extends React.Component {
     
     state = {
-        active: 'recipe'
+        active: 'recipe',
+        data:[
+            
+        ]
+    }
+
+    BASE_API_URL="https://8888-kunxinchor-dwadrecipeapi-tbknv7p66bz.ws-us34.gitpod.io/"
+
+    async componentDidMount() {
+        let response = await axios.get(this.BASE_API_URL + "recipes");
+        this.setState({
+            'data': response.data 
+        })
+
     }
 
     renderPage() {
         if (this.state.active === 'recipe') {
-            return <Listing/>
+            return <Listing data={this.state.data}/>
         } else if(this.state.active === 'add') {
-            return <AddNew/>
+            return <AddNew processAddNewRecipe={this.addRecipe}/>
         }
     }
 
     setActive(page) {
         this.setState({
             'active': page
+        })
+    }
+
+    // the newRecipe arugment should be an object with _id,
+    // title and ingredients
+    addRecipe = (newRecipe) => {
+        this.setState({
+            'data': [...this.state.data, newRecipe],
+            'active':'recipe'
         })
     }
 
